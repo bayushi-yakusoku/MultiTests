216eb020478da9c480aeb72962052b02b5875f31
Papa <bayushi.tonio@gmail.com>
2020-04-22 15:39:31 +0200

    Replace deprecated ViewModelProvider.of with delegation by viewModels()

--- app/src/main/java/alo/android/multitests/ui/home/HomeFragment.kt
+++ app/src/main/java/alo/android/multitests/ui/home/HomeFragment.kt
@@ -1,26 +1,24 @@
 package alo.android.multitests.ui.home
 
+import alo.android.multitests.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import alo.android.multitests.R
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private val homeViewModel : HomeViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
--- app/src/main/java/alo/android/multitests/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/alo/android/multitests/ui/slideshow/SlideshowFragment.kt
@@ -1,26 +1,24 @@
 package alo.android.multitests.ui.slideshow
 
+import alo.android.multitests.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import alo.android.multitests.R
 
 class SlideshowFragment : Fragment() {
 
-    private lateinit var slideshowViewModel: SlideshowViewModel
+    private val slideshowViewModel : SlideshowViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
         val textView: TextView = root.findViewById(R.id.text_slideshow)
         slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
