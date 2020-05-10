4d951c3444f0d89119ec541d22a7081e97310744
Papa <bayushi.tonio@gmail.com>
2020-05-10 17:43:43 +0200

    Add Card animation

--- app/src/main/java/alo/android/multitests/ui/flipcard/FlipCardFragment.kt
+++ app/src/main/java/alo/android/multitests/ui/flipcard/FlipCardFragment.kt
@@ -1,6 +1,9 @@
 package alo.android.multitests.ui.flipcard
 
+import alo.android.multitests.R
 import alo.android.multitests.databinding.FragmentFlipcardBinding
+import alo.android.multitests.tool.toast
+import android.animation.AnimatorInflater
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -16,8 +19,19 @@ class FlipCardFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        
         binding = FragmentFlipcardBinding.inflate(inflater, container, false)
+    
+        val animator = AnimatorInflater.loadAnimator(context, R.animator.simple_test)
+        
+        animator.setTarget(binding.cardImageView)
+        
+        binding.cardImageView.setOnClickListener {
+            context?.toast("Click")
+
+//            it.animate().alpha(0f).setDuration(1000)
+//            it.animate().rotationYBy(180f).setDuration(1000)
+            animator.start()
+        }
         
         return binding.root
     }
