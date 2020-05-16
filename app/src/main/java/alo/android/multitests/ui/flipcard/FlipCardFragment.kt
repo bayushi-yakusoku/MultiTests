e57bc23686e94efbe4142da38364fee369325340
Papa <bayushi.tonio@gmail.com>
2020-05-16 19:10:57 +0200

    Add front and back animation

--- app/src/main/java/alo/android/multitests/ui/flipcard/FlipCardFragment.kt
+++ app/src/main/java/alo/android/multitests/ui/flipcard/FlipCardFragment.kt
@@ -3,6 +3,7 @@ package alo.android.multitests.ui.flipcard
 import alo.android.multitests.R
 import alo.android.multitests.databinding.FragmentFlipcardBinding
 import alo.android.multitests.tool.toast
+import android.animation.Animator
 import android.animation.AnimatorInflater
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -14,25 +15,57 @@ class FlipCardFragment : Fragment() {
     
     lateinit var binding : FragmentFlipcardBinding
     
+    var isFront : Boolean = true
+    
+    lateinit var cardFrontAnimator : Animator
+    lateinit var cardBackAnimator : Animator
+    
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentFlipcardBinding.inflate(inflater, container, false)
+        isFront = true
     
-        val animator = AnimatorInflater.loadAnimator(context, R.animator.simple_test)
+        cardFrontAnimator = AnimatorInflater.loadAnimator(context, R.animator.front_card_animation)
+        cardBackAnimator = AnimatorInflater.loadAnimator(context, R.animator.back_card_animation)
         
-        animator.setTarget(binding.cardImageView)
+        binding = FragmentFlipcardBinding.inflate(inflater, container, false)
         
-        binding.cardImageView.setOnClickListener {
-            context?.toast("Click")
-
-//            it.animate().alpha(0f).setDuration(1000)
-//            it.animate().rotationYBy(180f).setDuration(1000)
-            animator.start()
+        binding.frontCardImageView.setOnClickListener {
+            context?.toast("Click on Front!")
+    
+            when (isFront) {
+                true -> frontToBackAnim()
+                false -> backToFrontAnim()
+            }
+        }
+        
+        binding.backCardImageView.setOnClickListener {
+            context?.toast("Click on Back!!")
         }
         
         return binding.root
+    }
+    
+    private fun frontToBackAnim() {
+        cardFrontAnimator.setTarget(binding.frontCardImageView)
+        cardBackAnimator.setTarget(binding.backCardImageView)
+        
+        cardFrontAnimator.start()
+        cardBackAnimator.start()
+        
+        isFront = false
+    }
+    
+    private fun backToFrontAnim() {
+    
+        cardFrontAnimator.setTarget(binding.backCardImageView)
+        cardBackAnimator.setTarget(binding.frontCardImageView)
+    
+        cardFrontAnimator.start()
+        cardBackAnimator.start()
+        
+        isFront = true
     }
 }
